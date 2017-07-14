3893cfb74e72d01df3b3d7bd1abae5fd4349637b
tradichel <teri.radichel@watchguard.com>
2017-07-14 09:48:07 -0700

    remove broken NTP rules until I have time to fix...

--- code/resources/firebox-lambda/fireboxcommands.py
+++ code/resources/firebox-lambda/fireboxcommands.py
@@ -22,13 +22,13 @@ class fireboxcommands:
     channel=None
     sshclient=None
     
-    logging.getLogger("paramiko").setLevel(logging.DEBUG) 
+    logging.getLogger('paramiko').setLevel(logging.DEBUG) 
 
     def __init__(self, bucket, sshkey, fireboxip):
 
         clone=self
 
-        local_key_file="/tmp/fb.pem"
+        local_key_file='/tmp/fb.pem'
 
         #For troubleshooting AWS requests if needed
         boto3.set_stream_logger(name='botocore')
@@ -38,10 +38,10 @@ class fireboxcommands:
         try:
             s3.download_file(bucket, sshkey, local_key_file)
         except:
-            print ("cannot connect to S3 Bucket")
+            print ('cannot connect to S3 Bucket')
             raise #last time this failed was DNS error
 
-        print ("try to connect to Firebox")
+        print ('try to connect to Firebox')
         try:
           
             k = paramiko.RSAKey.from_private_key_file(local_key_file)
@@ -49,14 +49,14 @@ class fireboxcommands:
             #override check in known hosts file
             #https://github.com/paramiko/paramiko/issues/340
             clone.sshclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            clone.sshclient.connect( hostname = fireboxip, port = 4118, username = "admin", key_filename = local_key_file)
+            clone.sshclient.connect( hostname = fireboxip, port = 4118, username = 'admin', key_filename = local_key_file)
             clone.channel = clone.sshclient.invoke_shell()
 
         except:
-            print ("cannot connect to firebox")
+            print ('cannot connect to firebox')
             raise
 
-        print("connected to " + fireboxip)
+        print('connected to ' + fireboxip)
 
     #run from main mode
     def check_exists(self, object, name ):
@@ -64,19 +64,19 @@ class fireboxcommands:
         exist=False
 
         try:
-            output = self.exe("show " + object + " " + name, False)
-            if(output.find("name")!=NOT_FOUND or output.find("escription")!=NOT_FOUND):
+            output = self.exe('show ' + object + ' ' + name, False)
+            if(output.find('name')!=NOT_FOUND or output.find('escription')!=NOT_FOUND):
                 exist=True
         except ValueError as err:
             output = ''.join(err.args)
-            if (output.find("not found")==NOT_FOUND and output.find("Fail to find")==NOT_FOUND):
+            if (output.find('not found')==NOT_FOUND and output.find('Fail to find')==NOT_FOUND):
                 #error is something other than not found
                 raise
 
         if (exist==True):
-            print(object + " " + name + " exists.")
+            print(object + ' ' + name + ' exists.')
         else:
-            print(object + " " + name + " does not exist.")
+            print(object + ' ' + name + ' does not exist.')
             #print(output)
 
         return exist      
@@ -86,9 +86,9 @@ class fireboxcommands:
         try:
             if (snatexists==True):
                 #this fails with snat in use
-                #self.delete("snat", snatname)
+                #self.delete('snat', snatname)
                 return
-            command="snat " + snatname + " static-nat " + addrtype + " " + extip + " " + intip + " port " + port
+            command='snat ' + snatname + ' static-nat ' + addrtype + ' ' + extip + ' ' + intip + ' port ' + port
             self.exe(command)
         except ValueError as err:
             if str(err.args).find('already exists')!=NOT_FOUND:
@@ -100,22 +100,22 @@ class fireboxcommands:
     def add_alias(self, name, description, aliastype, address, aliasexists):
         try:
             if aliasexists == True:
-                output=self.delete("alias", name)
-            if aliastype=="FQDN":
-                command="alias " + name + " " + aliastype + " \"" + address + "\""        
+                output=self.delete('alias', name)
+            if aliastype=='FQDN':
+                command='alias ' + name + ' ' + aliastype + ' \'' + address + '\''        
             else:
-                command="alias " + name + " " + aliastype + " " + address   
+                command='alias ' + name + ' ' + aliastype + ' ' + address   
             output=self.exe(command)
         except ValueError as err:
             print(output)
             print(err.args)
-            print("For now if alias is in use leave it...talking to engineering about this")
+            print('For now if alias is in use leave it...talking to engineering about this')
 
     #run from policy mode
     def add_rule_and_policy(self, policyname, protocol, port, rulename, addressfrom, addressto, addrtypefrom, addrtypeto, log, ruleexists):
         try:
             if ruleexists == True:
-                self.delete("rule", rulename)
+                self.delete('rule', rulename)
             self.add_policy(policyname, protocol, port)
             self.add_rule(rulename, policyname, addressfrom, addressto, addrtypefrom, addrtypeto, log, ruleexists)
         except ValueError as err:
@@ -124,9 +124,9 @@ class fireboxcommands:
     #run from policy mode
     def add_policy(self, policyname, protocol, port):
         try:
-            output = self.exe("policy-type " + policyname + " protocol " + protocol + " " + port)
+            output = self.exe('policy-type ' + policyname + ' protocol ' + protocol + ' ' + port)
             if output.find('already exists')!=-NOT_FOUND:
-                print("policy already exits")
+                print('policy already exits')
         except ValueError as err:
             if str(err.args).find('already exists')!=NOT_FOUND:
                 raise
@@ -134,25 +134,25 @@ class fireboxcommands:
                 print(err.args) 
 
     def delete(self, itemtype, name):
-        self.exe("no " + itemtype + " " + name)
+        self.exe('no ' + itemtype + ' ' + name)
 
     #run from policy mode
     def add_rule(self, rulename, policyname, addressfrom, addressto, addrtypefrom, addrtypeto, log, ruleexists):
         #remove existing rule and re-add it
         if ruleexists==True:
-            self.delete("rule", rulename)
-        self.exe("rule " + rulename)
+            self.delete('rule', rulename)
+        self.exe('rule ' + rulename)
         try:   
-            rule = "policy-type " + policyname + " from " + addrtypefrom + " " + addressfrom + " to " + addrtypeto + " " + addressto
-            if addrtypefrom=="alias" or addrtypeto=="alias":
-                rule = rule + " firewall allowed"
+            rule = 'policy-type ' + policyname + ' from ' + addrtypefrom + ' ' + addressfrom + ' to ' + addrtypeto + ' ' + addressto
+            if addrtypefrom=='alias' or addrtypeto=='alias':
+                rule = rule + ' firewall allowed'
             self.exe(rule)
             if log==True:
-                self.exe("logging log-message enable")
+                self.exe('logging log-message enable')
             self.apply() #if we got this far apply the changes
         except ValueError as err:
             if str(err.args).find('already exists')==NOT_FOUND:
-                print ("Raise error")
+                print ('Raise error')
             else:
                 print(err.args) 
         finally:
@@ -162,13 +162,13 @@ class fireboxcommands:
     def exe(self, command, printoutput=True):
 
         buff_size=2024
-        c=command + "\n"
+        c=command + '\n'
         self.channel.send(c)
 
         #wait for results to be buffered
         while not (self.channel.recv_ready()):
             if self.channel.exit_status_ready():
-                print ("Channel exiting. No data returned")
+                print ('Channel exiting. No data returned')
                 return
             time.sleep(3) 
 
@@ -188,47 +188,48 @@ class fireboxcommands:
     #this assumes NTP is not already enabled nad rules have
     #not been set
     def enable_ntp(self):
-        self.exe("ntp enable")
-        self.exe("ntp device-as-server enable")
+        output=''
+        self.exe('ntp enable')
+        self.exe('ntp device-as-server enable')
         #fix ntp rule to only allow NTP servers via DNS
         #these are not working currently...asking WG engineering team...
         self.policy()
-        try:
-            output = self.add_alias(ALIAS_NTP, "NTP Pool", "FQDN", "*.ntp.org", alias_ntp_exists)
+        #try:
+        #    output = self.add_alias(ALIAS_NTP, 'NTP Pool', 'FQDN', '*.ntp.org', alias_ntp_exists)
             #rule to allow Firebox to rach out to NTP servers
-            output = self.add_rule(RULE_NTP, POLICY_NTP, "Any-External", ALIAS_NTP, "alias", "alias", LOG_TRAFFIC, False)
-            #cmd.add_rule(RULE_NTP, POLICY_NTP, "Any-Trusted", "Any-External", "alias", "alias", LOG_TRAFFIC, True)
-            #cmd.add_rule(RULE_NTP, POLICY_NTP, "Any-Optional", "Any-External", "alias", "alias", LOG_TRAFFIC, False) #do not delete, update
-        except:
-            print("Error enabling NTP:")
-            print(output)
-        finally:
-            self.exit() #exit policy
+        #    output = self.add_rule(RULE_NTP, POLICY_NTP, 'Any-External', ALIAS_NTP, 'alias', 'alias', LOG_TRAFFIC, False)
+            #cmd.add_rule(RULE_NTP, POLICY_NTP, 'Any-Trusted', 'Any-External', 'alias', 'alias', LOG_TRAFFIC, True)
+            #cmd.add_rule(RULE_NTP, POLICY_NTP, 'Any-Optional', 'Any-External', 'alias', 'alias', LOG_TRAFFIC, False) #do not delete, update
+        #except:
+        #    print('Error enabling NTP:')
+        #    print(output)
+        #finally:
+        #    self.exit() #exit policy
             
     def enable_threat_intel(self):
-        self.exe("global-setting report-data enable")
+        self.exe('global-setting report-data enable')
 
     def configure(self):
-        self.exe("configure")
+        self.exe('configure')
 
     def policy(self):
-        self.exe("policy")
+        self.exe('policy')
 
     def apply(self):
-        self.exe("apply")
+        self.exe('apply')
 
     def exit(self):
         try:
-            self.exe("exit")
+            self.exe('exit')
         except:
-            print("Maybe one too many exits?")  
+            print('Maybe one too many exits?')  
             
     #always close connections in a finally block
     def close_connections(self):
         if self.channel:
             self.channel.close()
-            print ("channel closed")
+            print ('channel closed')
         
         if self.sshclient:
             self.sshclient.close()
-            print ("connection closed")
+            print ('connection closed')
